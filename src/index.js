8a4878ff73216c29078171a8c82e4f92798a3b5c
omfgtora <spaminol@gmail.com>
2019-05-15 15:35:53 -0700

    Scaffolding

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.scss';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Main from './pages/Main'
+import Bio from './pages/Bio'
+import Tips from './pages/Tips'
+import NotFound from './pages/NotFound'
+
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Main} />
+      <Route exact path="/bio" component={Bio} />
+      <Route exact path="/tips-and-tricks" component={Tips} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
