df1c82852307cbb7c155aafe5e9e7c3146a2ab1e
omfgtora <spaminol@gmail.com>
2019-05-18 18:03:27 -0700

    Links open in new tab

--- src/components/header/Links.jsx
+++ src/components/header/Links.jsx
@@ -5,10 +5,10 @@ export default function Links() {
   
   return (
     <div className="links">
-      <a href="https://github.com/omfgtora/"><FaGithub /></a>
-      <a href="https://www.linkedin.com/in/eroberts-me/"><FaLinkedin /></a>
-      <a href="https://dev.to/eroberts"><FaDev /></a>
-      <a href="https://twitter.com/eroberts_me"><FaTwitter /></a>
+      <a href="https://github.com/omfgtora/" target="_blank"><FaGithub /></a>
+      <a href="https://www.linkedin.com/in/eroberts-me/" target="_blank"><FaLinkedin /></a>
+      <a href="https://dev.to/eroberts" target="_blank"><FaDev /></a>
+      <a href="https://twitter.com/eroberts_me" target="_blank"><FaTwitter /></a>
     </div>
   )
 }
