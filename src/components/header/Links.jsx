e15489c59fbfd6883f613d51170f0f9415f311da
Ethan Roberts <9193319+erobertsme@users.noreply.github.com>
2023-08-19 12:20:21 -0700

    Update Github link in Links.jsx

--- src/components/header/Links.jsx
+++ src/components/header/Links.jsx
@@ -5,7 +5,7 @@ export default function Links() {
   
   return (
     <div className="links">
-      <a href="https://github.com/omfgtora/" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+      <a href="https://github.com/erobertsme/" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
       <a href="https://www.linkedin.com/in/eroberts-me/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
       <a href="https://dev.to/eroberts" target="_blank" rel="noopener noreferrer"><FaDev /></a>
       <a href="https://twitter.com/eroberts_me" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
