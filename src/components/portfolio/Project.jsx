2b93b0d65f26b677574466eedad42e29c46827b4
omfgtora <ethan@eroberts.me>
2023-08-19 13:55:58 -0700

    add rel=noreferrer

--- src/components/portfolio/Project.jsx
+++ src/components/portfolio/Project.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Project(props) {
 
   return (
-    <a className="project" href={props.data.url} target={ props.data.url ===  "/crypto" ? 'window' : '_blank' }>
+    <a className="project" href={props.data.url} target={ props.data.url ===  "/crypto" ? 'window' : '_blank'} rel="noopener noreferrer">
       {props.data.title}
     </a>
   )
